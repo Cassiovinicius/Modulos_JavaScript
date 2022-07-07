e1d3795ce7c831b1bec21b137c3fb804fc67888d
Cassiovinicius <cassio.alves7789@gmail.com>
2022-07-07 20:43:15 -0300

    modulos com javaScript no console

---
+++ funçoes.mjs
@@ -0,0 +1,11 @@
+function mostraidade(nome,idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+function mostracidade(nome,cidade) {
+    return `A cidade de ${nome} é ${cidade}`;
+}
+function mostrahobbie(nome,hobbie) {
+    return `O hobbie de ${nome} é ${hobbie}`;
+}
+
+export{mostraidade,mostracidade,mostrahobbie}
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraidade,mostracidade,mostrahobbie } from "./funçoes.mjs";
+
+console.log(mostraidade('Cássio',32)) 
+console.log(mostracidade('Cássio','contagem-MG'))
+console.log(mostrahobbie('Cássio','Futebol'))
